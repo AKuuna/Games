aa0e469631cd6fd2c9acea9b2451d896325a89f1
Alex Karlo <aleksandrakarlo2@gmail.com>
2023-02-04 18:47:35 +0100

    Marbles sketch

---
+++ AKmarbles/marbles/game.js
@@ -0,0 +1,76 @@
+/**
+ * NEEDED FUNCTIONS
+ * 1. PROPAGATE THE FIELD - firstly stable one f.e 6x6
+ * 2. ADD BALLS - firstly at propagation
+ * 3. ADD BALLS - after any ball movement
+ * 4. MOVE BALL TO CHOSEN FILE
+ * 5. CHECK IF THERE IS A LINE WINNING - make it dependable of lenght
+ * 6. DELETE WINNING BALLS FROM FIELD
+ * 7. CHECK IF ALL FIELD IS FULL - END GAME
+ */
+
+const GRID_SIZE = 8
+const TILE = [];
+
+function propagateTheBoard(){
+
+    
+        tile.element.addEventListener ( 'mouseover', () =>{
+        });
+    
+
+    getInitinalMarbles();
+};
+
+
+function getInitinalMarbles(){
+
+};
+
+
+function addNewMarbles(){
+
+};
+
+
+function chooseMarble(){
+
+};
+
+
+function moveMarble(){
+
+};
+
+
+function checkMarbleCombo(){
+
+};
+
+
+function deleteWinningMarbles(){
+
+};
+
+
+function addToScore(){
+
+};
+
+
+function checkIfFinished(){
+
+};
+
+
+function randomGridPosition() {
+    return{
+        x: Math.floor(Math.random() * GRID_SIZE) + 1,
+        y: Math.floor(Math.random() * GRID_SIZE) + 1
+    }
+};
+
+
+function colorThemeChange(){
+    
+};
---
+++ AKmarbles/marbles/userfriendly.js
@@ -0,0 +1,8 @@
+ export function styleBoardBright() {
+    document.body.style.backgroundColor = "lightgrey";
+    document.body.style.color="darkblue";
+    document.getElementById('game').style.backgroundColor = "lightgrey";
+    document.getElementById('board-game').style.backgroundColor = "white";
+    document.getElementById('menu').style.backgroundColor = "white";
+    document.getElementsByClassName('menu-text').style.color = "darkblue";
+  }
